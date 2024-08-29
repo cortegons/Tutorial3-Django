46dc936d56f813e0eb90abf4d073084c865b8d6d
cortegons <cortegons@eafit.edu.co>
2024-08-28 20:38:45 -0500

    agregada vista carrito

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import homePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView
+from .views import homePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView
 
 urlpatterns = [
     path("", homePageView.as_view(), name="home"),
@@ -8,5 +8,7 @@ urlpatterns = [
     path("products/", ProductIndexView.as_view(), name="index"),
     path("products/create", ProductCreateView.as_view(), name="form"),
     path("products/<str:id>", ProductShowView.as_view(), name="show"),
-    
+    path("cart/",  CartView.as_view(), name="cart_index"),
+    path("cart/add/<int:product_id>", CartView.as_view(), name="cart_add"),
+    path("cart/removeAll", CartRemoveAllView.as_view(), name="cart_removeAll")
 ]
